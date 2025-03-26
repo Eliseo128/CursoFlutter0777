1b42c0f140cabd811f7d497cf879414c459ca351
manuelnava <manuel.nava@cbtis128.edu.mx>
2025-03-26 18:46:45 +0000

    ahora si

---
+++ lib/mi_widgets/mi_tarjeta.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class LaTarjeta extends StatelessWidget {
+  const LaTarjeta({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        height: 150,
+        child: Card(
+          elevation: 20,
+          color: Colors.amber,
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Eliseo Nava 0777",
+                  style: TextStyle(color: Colors.black, fontSize: 24),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Tocame",
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue, // Fondo azul
+                    foregroundColor: Colors.white, // Texto blanco
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
